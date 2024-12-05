5eed659f23b30a08a3a83f6b91d9efadaf7a0adc
amanj-si <aman.jaiswal@sportzinteractive.net>
2024-12-05 19:32:24 +0530

    day-3

--- app/comments/[id]/route.ts
+++ app/comments/[id]/route.ts
@@ -1,10 +1,14 @@
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async  function GET(_request: Request,{params}:{
     params:{
         id:string
     }
 }){   
+    if(parseInt(params.id) > comments.length){
+        return redirect("/comments");
+    }
     const comment = comments.find(comment => comment.id === Number(params.id));
 
     return Response.json(comment);
--- app/profile/api/route.ts
+++ app/profile/api/route.ts
@@ -1,5 +1,24 @@
+import { type NextRequest } from "next/server";
+import { headers ,cookies } from "next/headers";
 
+export async  function GET(request:NextRequest){
+    const requestHeaders = new Headers(request.headers);
+    const headerList = headers();
 
-export async  function GET(){
-    return new Response("prfile world");
+    (await cookies()).set("resultPerPage","10");
+    const theme = request.cookies.get("theme");
+
+    console.log((await headerList).get("Authorization"));
+    console.log(requestHeaders.get("Authorization"));
+
+    console.log(theme);
+    console.log((await cookies()).get("resultPerPage"));
+
+    return new Response("<h1>profile world</h1>",{
+        headers:{
+            "Content-Type":"text/html",
+            "Set-Cookie":"theme=light"
+        }
+    }
+    );
 }
